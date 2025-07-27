751b3a3ef4de967465cfa41b1cc496f52b4b8890
Nayan <n007kingkong@gmail.com>
2025-07-28 00:41:17 +0600

    Problem 1  is solved

---
+++ prob-1.js
@@ -0,0 +1,17 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+const burgerPrice = 499;
+
+if(burgerPrice >= 500)
+{
+    console.log("Take your free drinks.");
+}
+else{
+    console.log("You have to pay 30tk for the drinks.");
+}
